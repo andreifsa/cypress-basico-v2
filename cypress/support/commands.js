571b72fcb16376d69bd99b82d02425ed756425cb
andreialves <andreifsa12@gmail.com>
2023-11-17 15:44:41 -0300

    Automation with Cypress:
    - Small changes on tests
    - Changes on "fillMandatoryFieldsAndSubmit" command
    - Small clean on repo

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,3 +6,9 @@ Cypress.Commands.add("fillMandatoryFieldsAndSubmit", () => {
     cy.get('button[type="submit"]').click()
     cy.get('.success').should('be.visible')
 });
+
+Cypress.Commands.add("typeLongText", () => {
+    const longText = 'Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test, Test'
+
+    cy.get('#open-text-area').type(longText, {delay: 0})
+});
